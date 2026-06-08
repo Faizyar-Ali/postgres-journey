e40aed2fd693828e08b8e249332f3493dd87d38e
Faizyar Ali <shaikhfaizy84@gmail.com>
2026-06-08 15:46:17 +0500

    Add: Data insertion in user table

--- Final SQL Readiness Challenge/Tables/users.sql
+++ Final SQL Readiness Challenge/Tables/users.sql
@@ -5,3 +5,11 @@ email varchar(255) unique not null,
 created_at timestamp default timezone('utc',now()) not null );
 CREATE TABLE
 learn=> 
+
+--Data insertion
+learn=> insert into users (username,email)
+values ('uzaif','uzaif032@gmail.com'),
+('maroof','maroof943@gmail.com');
+INSERT 0 2
+
+
