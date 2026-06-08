c265dc0eef2a8fc8d9b460ab06dee5a428ac95c7
Faizyar Ali <shaikhfaizy84@gmail.com>
2026-06-08 16:00:28 +0500

    Add: Projects table with data

---
+++ Final SQL Readiness Challenge/Tables/projects.sql
@@ -0,0 +1,18 @@
+learn=> create table projects(
+id serial primary key,
+name varchar(255) not null,
+owner_id int,
+created_at timestamp default timezone('utc',now()),
+foreign key (owner_id) References users(id));
+CREATE TABLE
+learn=> 
+
+--data insertion 
+learn=> insert into p
+projects  public.   
+learn=> insert into projects (name,owner_id)
+learn-> values ('SaaS App',1),
+learn-> ('CRM',1),
+learn-> ('Blog',2);
+INSERT 0 3
+
